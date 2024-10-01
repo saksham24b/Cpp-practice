fb2bcce849acac5c2078e839b9470f6db3adaaee
Saksham <saksham24b@gmail.com>
2024-10-01 12:14:33 +0530

    data types

--- 1.cpp
+++ 1.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main() {
     int age = 20;
-    cout << age;
+    cout << "Age: " << age << endl;
+    char grade = 'A';
+    cout << "Grade: " << grade << endl;
+    string name = "Saksham";
+    cout << "Name: " << name << endl;
+    float marks = 90.6f; // f is used to tell that it is a float
+    cout << "Float_Marks: " << marks << endl;
+    double marks2 = 95.6;
+    cout << "Double_Marks: " << marks2 << endl;
+    bool isMale = true;
+    cout << "Male(1/0): " << isMale << endl;
     return 0;
 }
