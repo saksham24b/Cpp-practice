abab4a5d712ff997570d61ae29b926590816fa1d
Saksham <saksham24b@gmail.com>
2024-10-03 22:59:36 +0530

    Sum of 2 numbers

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter 1st number: ";
+    cin>>a;
+
+    cout<<"Enter 2nd number: ";
+    cin>>b;
+
+    cout<<"Sum of "<<a<<" and "<<b<<" is "<<a+b<<endl;
+    return 0;
+}
