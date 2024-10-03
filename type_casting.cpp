bd2f08bd175b4beb85484e1922740e1236e10d81
Saksham <saksham24b@gmail.com>
2024-10-03 22:49:29 +0530

    Type Casting

---
+++ type_casting.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+// type casting is a way to convert a variable from one data type to another data type
+// output of the type casting is the same as the data type of the variable
+int main(){
+    int ans =(5/double(3)); // type casting 3 into double
+    cout << ans << endl; // output is 1 
+    return 0;
+}
