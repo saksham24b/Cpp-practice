04ad96888cc107da5817aa861a40b9cc083c5efe
Saksham <saksham24b@gmail.com>
2024-10-13 19:11:07 +0530

    Check prime number

---
+++ prime_num.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+
+    for(int i=2; i<n; i++){
+        if(n%i == 0){
+            cout << "Not a prime number";
+            break; // break the loop if the number is not prime
+        }
+        else{
+            cout << "Prime number";
+           break; 
+        }
+    }
+}
