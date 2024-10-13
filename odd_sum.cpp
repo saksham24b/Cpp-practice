e340a49f3461a936ddaf13b77ed8f9fe3e99620f
Saksham <saksham24b@gmail.com>
2024-10-13 18:55:27 +0530

    Sum of odd numbers

---
+++ odd_sum.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the last value n: ";
+    cin >> n;
+
+    int a;
+    cout << "Enter the starting value: ";
+    cin >> a;
+
+    int sum = 0;
+    for(int i=a; i<=n; i++){
+        if (i%2 != 0){
+            sum += i;
+        }
+    }
+    cout << "The sum of odd numbers from " << a << " to " << n << " is: " << sum << endl;
+}
